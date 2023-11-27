e655add6385f09917af26a63218884eaf1a00fb4
Parker M <99286695+p-maziarz@users.noreply.github.com>
2023-11-27 08:33:39 -0700

    Create MonteCarloSim.py

---
+++ MonteCarloSim.py
@@ -0,0 +1,12 @@
+# Import random package and call the function to output random value
+import random
+x = random.random()
+
+# Print results
+print (x)
+
+# Coin toss
+if x < 0.5:
+    print('Heads')
+else:
+    print('Tails')
